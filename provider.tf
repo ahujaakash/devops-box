d7869958543c2ad9c25e790ab07b0611a3a70fee
ahujaakash <aahuja888@gmail.com>
2020-01-22 13:42:04 +0530

    Add files via upload

---
+++ provider.tf
@@ -0,0 +1,7 @@
+provider "azurerm" {
+    version = "1.38.0"
+    subscription_id = "12690ce1-27b2-4482-a7ec-4953a220a9cf"
+    client_id = "c64cc615-622f-44f6-a868-b25f45b3467a"
+    tenant_id = "58268a60-6588-4971-a1b4-3ffe7753df00"
+    client_secret = "PEp[S==rTNg:0KmLm37GT5b47nqB.rTq"
+}
