3150e938898b85375a173201ee4b9b42f3a1ac65
ahujaakash <aahuja888@gmail.com>
2020-01-24 16:07:50 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,7 +1,12 @@
 provider "azurerm" {
     version = "1.38.0"
-    subscription_id = "12690ce1-27b2-4482-a7ec-4953a220a9cf"
-    client_id = "c64cc615-622f-44f6-a868-b25f45b3467a"
-    tenant_id = "58268a60-6588-4971-a1b4-3ffe7753df00"
-    client_secret = "PEp[S==rTNg:0KmLm37GT5b47nqB.rTq"
+ #   subscription_id = "12690ce1-27b2-4482-a7ec-4953a220a9cf"
+ #   client_id = "c64cc615-622f-44f6-a868-b25f45b3467a"
+ #   tenant_id = "58268a60-6588-4971-a1b4-3ffe7753df00"
+ #   client_secret = "PEp[S==rTNg:0KmLm37GT5b47nqB.rTq"
+    
+    subscription_id = "212f4c5d-22b2-4a62-98c7-8aaad03916af"
+    client_id = "3fc080b2-bd65-4057-bba9-e2a6e9e754d7"
+    tenant_id = "97e1bd48-db54-40a7-a903-800ca5f8d894"
+    client_secret = "_NrOKrnu6bwb?f3otj4_=jkctV0XG93c"
 }
